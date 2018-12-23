670e2cc7c21b7c8ef6ae8094ba1f9975bba3fdc5
Rocket Codester <45156318+RocketCodester@users.noreply.github.com>
2018-12-23 17:17:33 -0600

    Calculate the difference between dates

--- index.php
+++ index.php
@@ -13,6 +13,26 @@ $date = new DateTime('next week');
 $date = new DateTime('tomorrow');
 $date = DateTime::createFromFormat('d. m. Y', $row);
 
+$dvone = new DateTime('August 1, 1972');
+$spike = new DateTime('August 13, 1970');
+
+// Who is older?
+
+if ($dvone < $spike)
+{
+    echo '<p>D-Von is older than Spike</p>';
+}
+else
+{
+    echo '<p>Spike is older than D-Von</p>';
+}
+
+// Age Difference
+
+$diff = $dvone->diff($spike);
+
+echo $diff->format("<p>There is %y years, and %m months and %d days between Spike and D-Von</p>")
+
 ?>
 
 <p>The output date is: <?php echo $date->format('m/d/y') ?></p>
